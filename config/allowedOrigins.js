3b8a2757815f2957a8c65c3d6d969e3ca9b74086
Amalu Ekwelie <67154046+Prim3timer@users.noreply.github.com>
2026-06-17 21:16:27 -0400

    added git ignore

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
   "http://localhost:3001",
   "http://localhost:3500",
   "http://127.0.0.1:5500",
+  "https://erraback.onrender.com",
 ];
 
 module.exports = allowedOrigins;
